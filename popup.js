53fc71e2a6627005fed844c16ebaaad3e7a21106
Xavier Collantes <collantes.xavier@gmail.com>
2023-04-08 22:45:55 -0700

    Initial add of Chrome ext template
    Signed-off-by: Xavier Collantes <collantes.xavier@gmail.com>

---
+++ popup.js
@@ -0,0 +1,2 @@
+// Output found in extension window console.  
+// console.log("EXTENSION POPUP.JS")
---
+++ scripts/content.js
@@ -0,0 +1,24 @@
+// Output found in web pages's console. 
+console.log("STARTING CONTENT.JS");
+
+const insertPoint = document.querySelector("#GridgetContainer > div");
+
+if (insertPoint) {
+    chrome.runtime.sendMessage("get_url", response => {
+        const asin = getAsinFromUrl(response.tab.url);
+
+        const graphLink = `https://charts.camelcamelcamel.com/us/${asin}/amazon-new-used.png?legend=1&w=855&h=513&ilt=1&tp=all&fo=0&lang=en`;
+        const camelLink = `https://camelcamelcamel.com/product/${asin}`;
+        const altText = `Price graph found at ${camelLink}`;
+
+        const html = `<a href='${graphLink}'><img src='${graphLink}' alt='${altText}' /></a>`;
+        insertPoint.insertAdjacentHTML("afterend", html);
+    });
+}
+
+/**
+ * Extract the ASIN code from the Amazon.com link. 
+ */
+function getAsinFromUrl(url) {
+    return url.match(/\/dp\/([\d\w]+)\?/)[1];
+}
